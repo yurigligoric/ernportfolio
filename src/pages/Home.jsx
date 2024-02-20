80f78f9f4787d1526c7a9f19c957528937c83760
yurigligoric <yurigligoric@gmail.com>
2024-02-20 11:46:55 +0800

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import image from '../assets/ernbanawa.jpg'
+
+function Home() {
+  return (
+    <div>
+        <h1>Hi I&apos;m Ern</h1>    
+        <h1>Software and Game Developer</h1>
+        <img src={image} />
+        Home
+    </div>
+ 
+ 
+     )
+}
+
+export default Home
